c016803d5201acb287c71db223da9a6f31b21150
behemon <pluma54@gmail.com>
2019-03-11 15:02:42 +0200

    updating master to python 3

--- A_star_algorithm.py
+++ A_star_algorithm.py
@@ -1,10 +1,11 @@
 # A* Shortest Path Algorithm
 # http://en.wikipedia.org/wiki/A*
 # FB - 201012256
-from heapq import heappush, heappop # for priority queue
+from heapq import heappush, heappop  # for priority queue
 import math
 import time
 import random
+
 
 class node:
     # current position
@@ -13,18 +14,22 @@ class node:
     # total distance already travelled to reach the node
     distance = 0
     # priority = distance + remaining distance estimate
-    priority = 0 # smaller: higher priority
+    priority = 0  # smaller: higher priority
+
     def __init__(self, xPos, yPos, distance, priority):
         self.xPos = xPos
         self.yPos = yPos
         self.distance = distance
         self.priority = priority
-    def __lt__(self, other): # for priority queue
+
+    def __lt__(self, other):  # for priority queue
         return self.priority < other.priority
+
     def updatePriority(self, xDest, yDest):
-        self.priority = self.distance + self.estimate(xDest, yDest) * 10 # A*
+        self.priority = self.distance + self.estimate(xDest, yDest) * 10  # A*
+
     # give better priority to going straight instead of diagonally
-    def nextdistance(self, i): # i: direction
+    def nextdistance(self, i):  # i: direction
         if i % 2 == 0:
             self.distance += 10
         else:
@@ -40,37 +45,38 @@ class node:
         # d = abs(xd) + abs(yd)
         # Chebyshev distance
         # d = max(abs(xd), abs(yd))
-        return(d)
+        return (d)
+
 
 # A-star algorithm.
 # Path returned will be a string of digits of directions.
 def pathFind(the_map, directions, dx, dy, xStart, yStart, xFinish, yFinish, n, m):
-    closed_nodes_map = [] # map of closed (tried-out) nodes
-    open_nodes_map = [] # map of open (not-yet-tried) nodes
-    dir_map = [] # map of directions
+    closed_nodes_map = []  # map of closed (tried-out) nodes
+    open_nodes_map = []  # map of open (not-yet-tried) nodes
+    dir_map = []  # map of directions
     row = [0] * n
-    for i in range(m): # create 2d arrays
+    for i in range(m):  # create 2d arrays
         closed_nodes_map.append(list(row))
         open_nodes_map.append(list(row))
         dir_map.append(list(row))
 
-    pq = [[], []] # priority queues of open (not-yet-tried) nodes
-    pqi = 0 # priority queue index
+    pq = [[], []]  # priority queues of open (not-yet-tried) nodes
+    pqi = 0  # priority queue index
     # create the start node and push into list of open nodes
     n0 = node(xStart, yStart, 0, 0)
     n0.updatePriority(xFinish, yFinish)
     heappush(pq[pqi], n0)
-    open_nodes_map[yStart][xStart] = n0.priority # mark it on the open nodes map
+    open_nodes_map[yStart][xStart] = n0.priority  # mark it on the open nodes map
 
     # A* search
     while len(pq[pqi]) > 0:
         # get the current node w/ the highest priority
         # from the list of open nodes
-        n1 = pq[pqi][0] # top node
+        n1 = pq[pqi][0]  # top node
         n0 = node(n1.xPos, n1.yPos, n1.distance, n1.priority)
         x = n0.xPos
         y = n0.yPos
-        heappop(pq[pqi]) # remove the node from the open list
+        heappop(pq[pqi])  # remove the node from the open list
         open_nodes_map[y][x] = 0
         # mark it on the closed nodes map
         closed_nodes_map[y][x] = 1
@@ -83,7 +89,7 @@ def pathFind(the_map, directions, dx, dy, xStart, yStart, xFinish, yFinish, n, m
             path = ''
             while not (x == xStart and y == yStart):
                 j = dir_map[y][x]
-                c = str((j + directions / 2) % directions)
+                c = str((j + directions // 2) % directions)
                 path = c + path
                 x += dx[j]
                 y += dy[j]
@@ -93,7 +99,7 @@ def pathFind(the_map, directions, dx, dy, xStart, yStart, xFinish, yFinish, n, m
         for i in range(directions):
             xdx = x + dx[i]
             ydy = y + dy[i]
-            if not (xdx < 0 or xdx > n-1 or ydy < 0 or ydy > m - 1
+            if not (xdx < 0 or xdx > n - 1 or ydy < 0 or ydy > m - 1
                     or the_map[ydy][xdx] == 1 or closed_nodes_map[ydy][xdx] == 1):
                 # generate a child node
                 m0 = node(xdx, ydy, n0.distance, n0.priority)
@@ -104,12 +110,12 @@ def pathFind(the_map, directions, dx, dy, xStart, yStart, xFinish, yFinish, n, m
                     open_nodes_map[ydy][xdx] = m0.priority
                     heappush(pq[pqi], m0)
                     # mark its parent node direction
-                    dir_map[ydy][xdx] = (i + directions / 2) % directions
+                    dir_map[ydy][xdx] = (i + directions // 2) % directions
                 elif open_nodes_map[ydy][xdx] > m0.priority:
                     # update the priority info
                     open_nodes_map[ydy][xdx] = m0.priority
                     # update the parent direction info
-                    dir_map[ydy][xdx] = (i + directions / 2) % directions
+                    dir_map[ydy][xdx] = (i + directions // 2) % directions
                     # replace the node
                     # by emptying one pq to the other one
                     # except the node to be replaced will be ignored
@@ -117,93 +123,89 @@ def pathFind(the_map, directions, dx, dy, xStart, yStart, xFinish, yFinish, n, m
                     while not (pq[pqi][0].xPos == xdx and pq[pqi][0].yPos == ydy):
                         heappush(pq[1 - pqi], pq[pqi][0])
                         heappop(pq[pqi])
-                    heappop(pq[pqi]) # remove the wanted node
+                    heappop(pq[pqi])  # remove the wanted node
                     # empty the larger size pq to the smaller one
                     if len(pq[pqi]) > len(pq[1 - pqi]):
                         pqi = 1 - pqi
                     while len(pq[pqi]) > 0:
-                        heappush(pq[1-pqi], pq[pqi][0])
+                        heappush(pq[1 - pqi], pq[pqi][0])
                         heappop(pq[pqi])
                     pqi = 1 - pqi
-                    heappush(pq[pqi], m0) # add the better node instead
-    return '' # no route found
-if __name__== '__main__':
-    # MAIN
-    directions = 8 # number of possible directions to move on the map
-    if directions == 4:
-        dx = [1, 0, -1, 0]
-        dy = [0, 1, 0, -1]
-    elif directions == 8:
-        dx = [1, 1, 0, -1, -1, -1, 0, 1]
-        dy = [0, 1, 1, 1, 0, -1, -1, -1]
+                    heappush(pq[pqi], m0)  # add the better node instead
+    return ''  # no route found
 
-    # map matrix
-    n = 10 # horizontal size
-    m = 10 # vertical size
-    the_map = []
-    row = [0] * n
-    for i in range(m):
-        the_map.append(list(row))
-
-    # fillout the map matrix with a '+' pattern
-    for x in range(int(n / 8),int( n * 7 / 8)):
-        the_map[int(m / 2)][x] = 1
-    for y in range(int(m/8), int(m * 7 / 8)):
-        the_map[y][int(n / 2)] = 1
-
-    # randomly select start and finish locations from a list
-    sf = []
-    sf.append((0, 0, n - 1, m - 1))
-    sf.append((0, m - 1, n - 1, 0))
-    sf.append((n / 2 - 1, m / 2 - 1, n / 2 + 1, m / 2 + 1))
-    sf.append((n / 2 - 1, m / 2 + 1, n / 2 + 1, m / 2 - 1))
-    sf.append((n / 2 - 1, 0, n / 2 + 1, m - 1))
-    sf.append((n / 2 + 1, m - 1, n / 2 - 1, 0))
-    sf.append((0, m / 2 - 1, n - 1, m / 2 + 1))
-    sf.append((n - 1, m / 2 + 1, 0, m / 2 - 1))
-    (xA, yA, xB, yB) = random.choice(sf)
-
-    print( 'Map Size (X,Y): ', n, m)
-    print('Start: ', xA, yA)
-    print( 'Finish: ', xB, yB)
-    t = time.time()
-    route = pathFind(the_map, directions, dx, dy, int(xA), int(yA), int(xB), int(yB) ,n ,m)
-    print( 'Time to generate the route (s): ', time.time() - t)
-    print( 'Route:')
-    print( route)
-
-    # mark the route on the map
-    if len(route) > 0:
-        x = xA
-        y = yA
-        the_map[y][x] = 2
-        for i in range(len(route)):
-            j = int(route[i])
-            x += dx[j]
-            y += dy[j]
-            the_map[y][x] = 3
-        the_map[y][x] = 4
-
-    # display the map with the route
-    print ('Map:')
-    for y in range(m):
-        line = ""
-        for x in range(n):
-            xy = the_map[y][x]
-            if xy == 0:
-                # print ('.',) # space
-                line += ' . '
-            elif xy == 1:
-                # print ('O',) # obstacle
-                line += ' O '
-            elif xy == 2:
-                # print ('S',) # start
-                line += ' S '
-            elif xy == 3:
-                # print ('R',) # route
-                line += ' R '
-            elif xy == 4:
-                # print ('F',) # finish
-                line += ' F '
-        print(line)
-    raw_input('Press Enter...')
+# if (__name__ == "__main__"):
+# 	# MAIN
+# 	directions = 8 # number of possible directions to move on the map
+# 	if directions == 4:
+# 		dx = [1, 0, -1, 0]
+# 		dy = [0, 1, 0, -1]
+# 	elif directions == 8:
+# 		dx = [1, 1, 0, -1, -1, -1, 0, 1]
+# 		dy = [0, 1, 1, 1, 0, -1, -1, -1]
+#
+# 	# map matrix
+# 	n = 10 # horizontal size
+# 	m = 10 # vertical size
+# 	the_map = []
+# 	row = [0] * n
+# 	for i in range(m):
+# 		the_map.append(list(row))
+#
+# 	# fillout the map matrix with a '+' pattern
+# 	for x in range(n / 8, n * 7 / 8):
+# 		the_map[m / 2][x] = 1
+# 	for y in range(m/8, m * 7 / 8):
+# 		the_map[y][n / 2] = 1
+#
+# 	# randomly select start and finish locations from a list
+# 	sf = []
+# 	sf.append((0, 0, n - 1, m - 1))
+# 	sf.append((0, m - 1, n - 1, 0))
+# 	sf.append((n / 2 - 1, m / 2 - 1, n / 2 + 1, m / 2 + 1))
+# 	sf.append((n / 2 - 1, m / 2 + 1, n / 2 + 1, m / 2 - 1))
+# 	sf.append((n / 2 - 1, 0, n / 2 + 1, m - 1))
+# 	sf.append((n / 2 + 1, m - 1, n / 2 - 1, 0))
+# 	sf.append((0, m / 2 - 1, n - 1, m / 2 + 1))
+# 	sf.append((n - 1, m / 2 + 1, 0, m / 2 - 1))
+# 	(xA, yA, xB, yB) = random.choice(sf)
+#
+# 	print ('Map Size (X,Y): ', n, m)
+# 	print ('Start: ', xA, yA)
+# 	print ('Finish: ', xB, yB)
+# 	t = time.time()
+# 	route = pathFind(the_map, directions, dx, dy, xA, yA, xB, yB)
+# 	print ('Time to generate the route (s): ', time.time() - t)
+# 	print ('Route:')
+# 	print (route)
+#
+# 	# mark the route on the map
+# 	if len(route) > 0:
+# 		x = xA
+# 		y = yA
+# 		the_map[y][x] = 2
+# 		for i in range(len(route)):
+# 			j = int(route[i])
+# 			x += dx[j]
+# 			y += dy[j]
+# 			the_map[y][x] = 3
+# 		the_map[y][x] = 4
+#
+# 	# display the map with the route
+# 	print ('Map:')
+# 	for y in range(m):
+# 		for x in range(n):
+# 			xy = the_map[y][x]
+# 			if xy == 0:
+# 				print ('.',) # space
+# 			elif xy == 1:
+# 				print ('O',) # obstacle
+# 			elif xy == 2:
+# 				print ('S',) # start
+# 			elif xy == 3:
+# 				print ('R',) # route
+# 			elif xy == 4:
+# 				print ('F',) # finish
+# 		print
+#
+# 	raw_input('Press Enter...')
